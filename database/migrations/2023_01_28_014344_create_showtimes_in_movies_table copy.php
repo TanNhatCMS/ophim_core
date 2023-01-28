bb475fe01017bf9098cd7864bfea7f1d6e0c99bd
TanNhatCMS <mrkiet.edu@gmail.com>
2023-01-28 16:09:03 +0700

    Create 2023_01_28_014344_create_showtimes_in_movies_table copy.php

---
+++ database/migrations/2023_01_28_014344_create_showtimes_in_movies_table copy.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('movies', function (Blueprint $table) {
+            $table->boolean('is_shown_in_weekly')->default(false);
+            $table->string('showntimes_in_weekly', 5)->nullable();
+            $table->boolean('showntimes_in_weekday',1)->default(0);
+            $table->string('showntimes_in_day', 20)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('movies', function (Blueprint $table) {
+            $table->dropColumn('is_shown_in_weekly');
+            $table->dropColumn('showntimes_in_weekly');
+            $table->dropColumn('showntimes_in_weekday');
+            $table->dropColumn('showntimes_in_day');
+        });
+    }
+};
