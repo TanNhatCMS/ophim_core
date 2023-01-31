e26048fac1162fc82eab4258130eb8a719566900
TanNhatCMS <mrkiet.edu@gmail.com>
2023-02-01 04:13:18 +0700

    Update topbar_right_content.blade.php

--- resources/views/core/base/inc/topbar_right_content.blade.php
+++ resources/views/core/base/inc/topbar_right_content.blade.php
@@ -8,7 +8,7 @@
         <a class="dropdown-item" href="{{ backpack_url('quick-action/delete-cache') }}">
             Xoá tất cả Cache
         </a>
-        @if (\Composer\InstalledVersions::isInstalled("appstract/laravel-opcache"))
+        @if (!\Composer\InstalledVersions::isInstalled("appstract/laravel-opcache"))
             <div class="dropdown-divider"></div>
             <a class="dropdown-item" href="{{ backpack_url('quick-action/delete-opcache') }}">Xoá OpCache</a>
         @endif
