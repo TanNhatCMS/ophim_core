5e50911456a04531831946c970b3961a5b257dfa
TanNhatCMS <mrkiet.edu@gmail.com>
2023-02-03 10:14:51 +0700

    Update column_movie_info.blade.php

--- resources/views/core/movies/columns/column_movie_info.blade.php
+++ resources/views/core/movies/columns/column_movie_info.blade.php
@@ -32,17 +32,25 @@ $config_show_status = [
 ];
 @endphp
     @includeWhen(!empty($column['wrapper']), 'crud::columns.inc.wrapper_start')
-        <div style="display: flex; width: 250px;" class="">
+    <div class="card mb-3" style=" max-width: 350px; height:250px; ">
+        <div  style="display: flex;" >
+            <div class="col-md-4">
                 <img src="{{ $thumb_url }}" height="100px" width="68px"/>
-                <div style="width: 150px; overflow-x: auto;">
-                    <div style="width: 150px;" class="text-primary pb-2">{{ $name }} </div>
-                    <div tyle="width: 150px;" class="text-muted pb-2">
-                        <small>({{ $origin_name }})</small>
-                    </div>
-                    <span class="text-success">[{{ $publish_year }}]</span>
-                    <span class="text-danger">[{{ $episode_current }}]</span><br/>
-                    <div class="badge {{ $config_show_type[$movie_type]['class'] }} font-weight-normal">{{ $config_show_type[$movie_type]['label'] }}</div>
-                    <div class="badge {{ $config_show_status[$status]['class'] }} font-weight-normal">{{ $config_show_status[$status]['label'] }}</div>
+            </div>
+            <div class="col-md-8">
+                <div class="card-body" >
+                    <h5 class="card-title text-primary pb-2">{{ $name }}</h5>
+                    <h6 class="card-title text-muted pb-2">({{ $origin_name }})</h6>
+                    <p class="card-text">
+                        <span class="text-success">[{{ $publish_year }}]</span>
+                        <span class="text-danger">[{{ $episode_current }}]</span>
+                    </p>
+                    <p class="card-text">
+                        <div class="badge {{ $config_show_type[$movie_type]['class'] }} font-weight-normal">{{ $config_show_type[$movie_type]['label'] }}</div>
+                        <div class="badge {{ $config_show_status[$status]['class'] }} font-weight-normal">{{ $config_show_status[$status]['label'] }}</div>
+                    </p>
                 </div>
+            </div>
         </div>
+    </div>
     @includeWhen(!empty($column['wrapper']), 'crud::columns.inc.wrapper_end')
