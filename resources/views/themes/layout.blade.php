c1781f654035bb7976c1360e5268a888a47fb077
TanNhatCMS <mrkiet.edu@gmail.com>
2023-02-18 20:03:13 +0700

    Update layout.blade.php

--- resources/views/themes/layout.blade.php
+++ resources/views/themes/layout.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="{{ config('app.locale') }}">
-
+<html prefix="og: http://ogp.me/ns#" xmlns="http://www.w3.org/1999/xhtml" xml:lang="{{ config('app.locale') }}" lang="{{ config('app.locale') }}">
 <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta charSet="utf-8" >
-    <meta name="viewport" content="initial-scale=1.0, width=device-width" >
-
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
+    <meta http-equiv="Content-Language" content="{{ config('app.locale') }}" />
     <meta name="robots" content="index,follow" >
     <meta name="revisit-after" content="1 days" >
     <meta name="ROBOTS" content="index,follow" >
@@ -13,6 +13,8 @@
     <meta name="yahooBOT" content="index,follow" >
     <meta name="slurp" content="index,follow" >
     <meta name="msnbot" content="index,follow" >
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta property="fb:app_id" content="{{ setting('social_facebook_app_id') }}" >
     <link rel="shortcut icon" href="{{ setting('site_meta_shortcut_icon') }}" type="image/png" >
