db6a1253ce983a6fb8b11767ecc79f18366ba324
TanNhatCMS <mrkiet.edu@gmail.com>
2023-05-03 02:35:44 +0700

    sửa lỗi tiếng Việt

--- src/Models/Theme.php
+++ src/Models/Theme.php
@@ -59,7 +59,7 @@ class Theme extends Model implements Cacheable
 
     public function editBtn($crud = false)
     {
-        return '<a href="' . backpack_url("theme/{$this->id}/edit") . '" class="btn btn-primary">Edit</a>';
+        return '<a href="' . backpack_url("theme/{$this->id}/edit") . '" class="btn btn-primary">Cài đặt</a>';
     }
 
     public function deleteBtn($crud = false)
@@ -69,7 +69,7 @@ class Theme extends Model implements Cacheable
             $template = <<<EOT
             <form action="{actionRoute}" method="post" onsubmit="return confirm('Chắc chắn muốn xóa giao diện {display_name}?. ');" style="display: inline">
                 {csrfField}
-                <button class="btn btn-danger" type="submit">Delete</button>
+                <button class="btn btn-danger" type="submit">Xoá</button>
             </form>
             EOT;
             $html = str_replace("{actionRoute}", backpack_url("theme/{$this->id}/delete"), $template);
@@ -95,10 +95,10 @@ class Theme extends Model implements Cacheable
         $html = str_replace("{display_name}", $this->display_name, $html);
 
         if ($this->active) {
-            $html = str_replace("{name}", 'Re-Activate', $html);
+            $html = str_replace("{name}", 'Kích hoạt lại', $html);
             $html = str_replace("{btnType}", 'btn-secondary', $html);
         } else {
-            $html = str_replace("{name}", 'Activate', $html);
+            $html = str_replace("{name}", 'Kích hoạt', $html);
             $html = str_replace("{btnType}", 'btn-primary', $html);
         }
 
@@ -117,7 +117,7 @@ class Theme extends Model implements Cacheable
         $html = str_replace("{actionRoute}", backpack_url("theme/{$this->id}/reset"), $template);
         $html = str_replace("{csrfField}", csrf_field(), $html);
         $html = str_replace("{btnType}", 'btn-warning', $html);
-        $html = str_replace("{name}", 'Reset', $html);
+        $html = str_replace("{name}", 'Thiết lập lại', $html);
 
         return $html;
     }
